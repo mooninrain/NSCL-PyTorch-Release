7aef139aa7d5fca3c94d2cf5572b60a88108c735
Ruidong Wu <mooninrain@outlook.com>
2019-11-08 07:29:43 -0500

    Update monet_model.py

--- nscl/nn/monet/monet_model.py
+++ nscl/nn/monet/monet_model.py
@@ -9,27 +9,6 @@ from . import networks
 
 
 class MONetModel(BaseModel):
-
-    @staticmethod
-    def modify_commandline_options(parser, is_train=True):
-        """Add new model-specific options and rewrite default values for existing options.
-
-        Parameters:
-            parser -- the option parser
-            is_train -- if it is training phase or test phase. You can use this flag to add training-specific or test-specific options.
-
-        Returns:
-            the modified parser.
-        """
-        parser.set_defaults(batch_size=64, lr=1e-4, display_ncols=7, niter_decay=0,
-                            dataset_mode='clevr', niter=int(64e6 // 7e4))
-        parser.add_argument('--num_slots', metavar='K', type=int, default=7, help='Number of supported slots')
-        parser.add_argument('--z_dim', type=int, default=16, help='Dimension of individual z latent per slot')
-        if is_train:
-            parser.add_argument('--beta', type=float, default=0.5, help='weight for the encoder KLD')
-            parser.add_argument('--gamma', type=float, default=0.5, help='weight for the mask KLD')
-        return parser
-
     def __init__(self, opt):
         """Initialize this model class.
 
@@ -41,14 +20,20 @@ class MONetModel(BaseModel):
         - define loss function, visualization images, model names, and optimizers
         """
         BaseModel.__init__(self, opt)  # call the initialization method of BaseModel
+
+        self.beta = 0.5
+        self.gamma = 0.5
+        self.num_slots = 11
+
+
         self.loss_names = ['E', 'D', 'mask']
-        self.visual_names = ['m{}'.format(i) for i in range(opt.num_slots)] + \
-                            ['x{}'.format(i) for i in range(opt.num_slots)] + \
-                            ['xm{}'.format(i) for i in range(opt.num_slots)] + \
+        self.visual_names = ['m{}'.format(i) for i in range(self.num_slots)] + \
+                            ['x{}'.format(i) for i in range(self.num_slots)] + \
+                            ['xm{}'.format(i) for i in range(self.num_slots)] + \
                             ['x', 'x_tilde']
         self.model_names = ['Attn', 'CVAE']
-        self.netAttn = networks.init_net(networks.Attention(opt.input_nc, 1), gpu_ids=self.gpu_ids)
-        self.netCVAE = networks.init_net(networks.ComponentVAE(opt.input_nc, opt.z_dim), gpu_ids=self.gpu_ids)
+        self.netAttn = networks.init_net(networks.Attention(3, 1), gpu_ids=self.gpu_ids)
+        self.netCVAE = networks.init_net(networks.ComponentVAE(3, 16), gpu_ids=self.gpu_ids)
         self.eps = torch.finfo(torch.float).eps
         # define networks; you can use opt.isTrain to specify different behaviors for training and test.
         if self.isTrain:  # only defined during training time
@@ -78,9 +63,9 @@ class MONetModel(BaseModel):
         shape[1] = 1
         log_s_k = self.x.new_zeros(shape)
 
-        for k in range(self.opt.num_slots):
+        for k in range(self.num_slots):
             # Derive mask from current scope
-            if k != self.opt.num_slots - 1:
+            if k != self.num_slots - 1:
                 log_alpha_k = self.netAttn(self.x, log_s_k)
                 log_m_k = log_s_k + log_alpha_k
                 # Compute next scope
@@ -116,18 +101,14 @@ class MONetModel(BaseModel):
         self.m = torch.cat(m, dim=1)
         self.m_tilde_logits = torch.cat(m_tilde_logits, dim=1)
 
+    return self.m
+
     def backward(self):
         """Calculate losses, gradients, and update network weights; called in every training iteration"""
         n = self.x.shape[0]
         self.loss_E /= n
         self.loss_D = -torch.logsumexp(self.b, dim=1).sum() / n
         self.loss_mask = self.criterionKL(self.m_tilde_logits.log_softmax(dim=1), self.m)
-        loss = self.loss_D + self.opt.beta * self.loss_E + self.opt.gamma * self.loss_mask
-        loss.backward()
+        loss = self.loss_D + self.beta * self.loss_E + self.gamma * self.loss_mask
 
-    def optimize_parameters(self):
-        """Update network weights; it will be called in every training iteration."""
-        self.forward()               # first call forward to calculate intermediate results
-        self.optimizer.zero_grad()   # clear network G's existing gradients
-        self.backward()              # calculate gradients for network G
-        self.optimizer.step()        # update gradients for network G
+        return loss
