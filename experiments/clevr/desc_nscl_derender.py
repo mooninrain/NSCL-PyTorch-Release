948b8e0cd17f91922e7ce69455b7806a192ecc8e
Ruidong Wu <mooninrain@outlook.com>
2019-11-18 18:58:38 -0500

    Update desc_nscl_derender.py

--- experiments/clevr/desc_nscl_derender.py
+++ experiments/clevr/desc_nscl_derender.py
@@ -40,7 +40,6 @@ class Model(ReasoningV1Model):
 
         f_scene = self.resnet(feed_dict.image)
         f_sng = self.scene_graph(f_scene, feed_dict.objects, feed_dict.objects_length)
-        import pdb; pdb.set_trace()
 
         programs = feed_dict.program_qsseq
         programs, buffers, answers = self.reasoning(f_sng, programs, fd=feed_dict)
