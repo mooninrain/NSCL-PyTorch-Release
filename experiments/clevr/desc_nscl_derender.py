eb4bd984c59f2c936e1d1990f31f6794316af547
Ruidong Wu <mooninrain@outlook.com>
2019-11-18 16:54:38 -0500

    Update desc_nscl_derender.py

--- experiments/clevr/desc_nscl_derender.py
+++ experiments/clevr/desc_nscl_derender.py
@@ -22,7 +22,7 @@ from jacinle.utils.container import GView
 from nscl.models.reasoning_v1 import make_reasoning_v1_configs, ReasoningV1Model
 from nscl.models.utils import canonize_monitors, update_from_loss_module
 
-from nscl.nn.monet.monet_model import MONetModel
+from nscl.nn.monet.monet_model import MONet
 
 configs = make_reasoning_v1_configs()
 configs.model.vse_known_belong = False
