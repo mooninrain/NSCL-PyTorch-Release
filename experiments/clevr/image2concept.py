30ccf1260756867379f1f3b643f8fae34921eae9
Ruidong Wu <mooninrain@outlook.com>
2019-11-19 16:45:29 -0500

    Update image2concept.py

--- experiments/clevr/image2concept.py
+++ experiments/clevr/image2concept.py
@@ -21,7 +21,6 @@ Note that, in order to train this model, one must use the curriculum learning.
 from jacinle.utils.container import GView
 from nscl.models.image2concept_v1 import make_reasoning_v1_configs, Im2ConceptV1Model
 from nscl.models.utils import canonize_monitors, update_from_loss_module
-from nscl.nn.monet.utils import resize_module
 
 configs = make_reasoning_v1_configs()
 configs.model.vse_known_belong = False
