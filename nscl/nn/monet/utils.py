b5bc08ea0355f0e2f113c91ca9e686e889ebc568
Ruidong Wu <mooninrain@outlook.com>
2019-11-19 16:43:28 -0500

    Update utils.py

--- nscl/nn/monet/utils.py
+++ nscl/nn/monet/utils.py
@@ -1,10 +1,11 @@
 import torch
 import torch.nn as nn
 import jactorch.nn as jacnn
+import torchvision.transforms as transforms
 
 class resize_module(nn.Module):
     def __init__(self,h1,w1,h2,w2):
-        super().__init__()
+        super(resize_module).__init__()
         self.h1, self.w1 = h1, w1
         self.h2, self.w2 = h2, w2
         self.batch_dim = batch_dim
@@ -12,11 +13,27 @@ class resize_module(nn.Module):
         self._type_defined_ = False
 
     def forward(self,input):
-        # input [batch_size, h1, w1]
+        # input [batch_size, n_channel, h1, w1]
 
         if not self._type_defined_:
             ind = torch.arange(input.shape[0],dtype=input.dtype,device=input.device).unsqueeze(-1)
             box = torch.tensor([0,0,self.h1-1,self.w1-1],dtype=input.dtype,device=input.device).unsqueeze(0).repeat(input.shape[0],1)
             self.ind_and_box = torch.cat([ind, box], dim=-1)
             self._type_defined_ = True
-        return self.roi_pool(input,self.ind_and_box).squeeze() # output [batch_size, h2, w2]
+        return self.roi_pool(input,self.ind_and_box).squeeze() # output [batch_size, n_channel, h2, w2]
+
+class resize_module_cv2(nn.Module):
+    def __init__(self,h1,w1,h2,w2):
+        super(resize_module_cv2).__init__()
+        self.transform = transforms.Compose([
+            transforms.ToPILImage(),
+            transforms.Resize(size=(h2,w2)),
+            transforms.ToTensor()
+            ])
+
+    def forward(self,input):
+        # input [batch_size, n_channel, h1, w1]
+        output = []
+        for i in range(input.shape[0]):
+            output.append(self.transform(input[i]))
+        return torch.stack(output,dim=0) # output [batch_size, n_channel, h2, w2]
