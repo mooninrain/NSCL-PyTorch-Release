1cc53bb6a91afce0e80ed4926208fd9baa6d7984
Ruidong Wu <mooninrain@outlook.com>
2019-11-21 16:00:56 -0500

    Update test.py

--- scripts/test.py
+++ scripts/test.py
@@ -1,6 +1,89 @@
-a=0
-b=1
-c=2
-import pdb; pdb.set_trace()
-d=3
-print(a,b,c,d)
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+# File   : trainval.py
+# Author : Jiayuan Mao
+# Email  : maojiayuan@gmail.com
+# Date   : 10/05/2018
+#
+# This file is part of NSCL-PyTorch.
+# Distributed under terms of the MIT license.
+
+"""
+Training and evaulating the Neuro-Symbolic Concept Learner.
+"""
+
+import time
+import os.path as osp
+
+import torch.backends.cudnn as cudnn
+import torch.cuda as cuda
+
+from jacinle.cli.argument import JacArgumentParser
+from jacinle.logging import get_logger, set_output_file
+from jacinle.utils.imp import load_source
+from jacinle.utils.tqdm import tqdm_pbar
+
+from jactorch.cli import escape_desc_name, ensure_path, dump_metainfo
+from jactorch.cuda.copy import async_copy_to
+from jactorch.train import TrainerEnv
+from jactorch.utils.meta import as_float
+
+from nscl.datasets import get_available_datasets, initialize_dataset, get_dataset_builder
+
+parser = JacArgumentParser(description=__doc__.strip())
+
+parser.add_argument('--dataset_name', required=True,choices=[])
+parser.add_argument('--desc', required=True, type='checked_file', metavar='FILE')
+parser.add_argument('--configs', default='', type='kv', metavar='CFGS')
+
+# training_target and curriculum learning
+parser.add_argument('--expr', required=True, metavar='DIR', help='experiment name')
+parser.add_argument('--training-target', required=True, choices=['derender', 'parser', 'all'])
+parser.add_argument('--training-visual-modules', default='all', choices=['none', 'object', 'relation', 'all'])
+parser.add_argument('--curriculum', default='all', choices=['off', 'scene', 'program', 'all'])
+parser.add_argument('--question-transform', default='off', choices=['off', 'basic', 'parserv1-groundtruth', 'parserv1-candidates', 'parserv1-candidates-executed'])
+parser.add_argument('--concept-quantization-json', default=None, metavar='FILE')
+
+# running mode
+parser.add_argument('--debug', action='store_true', help='debug mode')
+parser.add_argument('--evaluate', action='store_true', help='run the validation only; used with --resume')
+
+# training hyperparameters
+parser.add_argument('--epochs', type=int, default=10, metavar='N', help='number of total epochs to run')
+parser.add_argument('--enums-per-epoch', type=int, default=1, metavar='N', help='number of enumerations of the whole dataset per epoch')
+parser.add_argument('--batch-size', type=int, default=64, metavar='N', help='batch size')
+parser.add_argument('--lr', type=float, default=0.001, metavar='N', help='initial learning rate')
+parser.add_argument('--iters-per-epoch', type=int, default=0, metavar='N', help='number of iterations per epoch 0=one pass of the dataset (default: 0)')
+parser.add_argument('--acc-grad', type=int, default=1, metavar='N', help='accumulated gradient (default: 1)')
+parser.add_argument('--clip-grad', type=float, metavar='F', help='gradient clipping')
+parser.add_argument('--validation-interval', type=int, default=1, metavar='N', help='validation inverval (epochs) (default: 1)')
+
+# finetuning and snapshot
+parser.add_argument('--load', type='checked_file', default=None, metavar='FILE', help='load the weights from a pretrained model (default: none)')
+parser.add_argument('--resume', type='checked_file', default=None, metavar='FILE', help='path to latest checkpoint (default: none)')
+parser.add_argument('--start-epoch', type=int, default=0, metavar='N', help='manual epoch number')
+parser.add_argument('--save-interval', type=int, default=2, metavar='N', help='model save interval (epochs) (default: 10)')
+
+# data related
+parser.add_argument('--dataset', required=True, choices=get_available_datasets(), help='dataset')
+parser.add_argument('--dataset', required=True, choices=(), help='dataset')
+parser.add_argument('--data-dir', required=True, type='checked_dir', metavar='DIR', help='data directory')
+parser.add_argument('--data-trim', type=float, default=0, metavar='F', help='trim the dataset')
+parser.add_argument('--data-split',type=float, default=0.75, metavar='F', help='fraction / numer of training samples')
+parser.add_argument('--data-vocab-json', type='checked_file', metavar='FILE')
+parser.add_argument('--data-scenes-json', type='checked_file', metavar='FILE')
+parser.add_argument('--data-questions-json', type='checked_file', metavar='FILE', nargs='+')
+
+parser.add_argument('--extra-data-dir', type='checked_dir', metavar='DIR', help='extra data directory for validation')
+parser.add_argument('--extra-data-scenes-json', type='checked_file', nargs='+', default=None, metavar='FILE', help='extra scene json file for validation')
+parser.add_argument('--extra-data-questions-json', type='checked_file', nargs='+', default=None, metavar='FILE', help='extra question json file for validation')
+
+parser.add_argument('--data-workers', type=int, default=4, metavar='N', help='the num of workers that input training data')
+
+# misc
+parser.add_argument('--use-gpu', type='bool', default=True, metavar='B', help='use GPU or not')
+parser.add_argument('--use-tb', type='bool', default=False, metavar='B', help='use tensorboard or not')
+parser.add_argument('--embed', action='store_true', help='entering embed after initialization')
+parser.add_argument('--force-gpu', action='store_true', help='force the script to use GPUs, useful when there exists on-the-ground devices')
+
+args = parser.parse_args()
