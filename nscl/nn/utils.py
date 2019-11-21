6ac0e48d89a75403bec031d55b8acd71ade0b5b9
Ruidong Wu <mooninrain@outlook.com>
2019-11-21 16:00:51 -0500

    Create utils.py

---
+++ nscl/nn/utils.py
@@ -0,0 +1,10 @@
+
+import numpy as np
+import torch
+
+def get_memory(model,_print_=True):
+    para = sum([np.prod(list(p.size())) for p in model.parameters()])
+    if _print_:
+        print(model)
+        print('Model {} : params: {:4f}M'.format(model.__class__.__name__, para * 4 / 1024 / 1024))
+    return para
