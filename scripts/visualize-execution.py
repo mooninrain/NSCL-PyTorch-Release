7ca2a95ffa43909f91e6585ca22ae61fe4bc59af
Ruidong Wu <mooninrain@outlook.com>
2019-11-22 01:55:36 -0500

    Update visualize-execution.py

--- scripts/visualize-execution.py
+++ scripts/visualize-execution.py
@@ -217,10 +217,10 @@ def validate_epoch(epoch, model, val_dataloader, meters, meter_prefix='validatio
                         else:
                             num_slots = output_dict['monet/m'].shape[1]
                             monet_fig = [
-                            [TF.to_pil_image(output_dict['monet/m'][i,k]) for k in range(num_slots)],
-                            [TF.to_pil_image(output_dict['monet/x'][i,k]) for k in range(num_slots)],
-                            [TF.to_pil_image(output_dict['monet/xm'][i,k]) for k in range(num_slots)],
-                            [TF.to_pil_image(output_dict['monet/x_tilde'][i]) for k in range(num_slots)]
+                            [tensor2im(output_dict['monet/m'][i,k]) for k in range(num_slots)],
+                            [tensor2im(output_dict['monet/x'][i,k]) for k in range(num_slots)],
+                            [tensor2im(output_dict['monet/xm'][i,k]) for k in range(num_slots)],
+                            [tensor2im(output_dict['monet/x_tilde'][i]) for k in range(num_slots)]
                             ]
                             montage = montage_fig(monet_fig)
                         vis.row(id=i, image=fig, mask=montage)
@@ -275,6 +275,26 @@ def montage_fig(images, fig_size=(11,4),ncols=11,nrows=4):
             ax[x,y].imshow(images[x][y])
             ax[x,y].axis('off')
     return fig
+
+def tensor2im(input_image, imtype=np.uint8):
+    """"Converts a Tensor array into a numpy image array.
+
+    Parameters:
+        input_image (tensor) --  the input image tensor array
+        imtype (type)        --  the desired type of the converted numpy array
+    """
+    if not isinstance(input_image, np.ndarray):
+        if isinstance(input_image, torch.Tensor):  # get the data from a variable
+            image_tensor = input_image.data
+        else:
+            return input_image
+        image_numpy = image_tensor[0].cpu().float().numpy()  # convert it into a numpy array
+        if image_numpy.shape[0] == 1:  # grayscale to RGB
+            image_numpy = np.tile(image_numpy, (3, 1, 1))
+        image_numpy = (np.transpose(image_numpy, (1, 2, 0)) + 1) / 2.0 * 255.0  # post-processing: tranpose and scaling
+    else:  # if it is a numpy array, do nothing
+        image_numpy = input_image
+    return Image.fromarray(image_numpy.astype(imtype))
 
 # def tensor_to_fig(tensor,_permute=True):
 #     if _permute:
