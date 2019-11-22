38f228b1eb5ed543f86bb4de5a38851dc4cf2578
Ruidong Wu <mooninrain@outlook.com>
2019-11-22 01:40:29 -0500

    Update visualize-execution.py

--- scripts/visualize-execution.py
+++ scripts/visualize-execution.py
@@ -267,13 +267,11 @@ def validate_epoch(epoch, model, val_dataloader, meters, meter_prefix='validatio
 
     logger.info('Happy Holiday! You can find your result at "http://monday.csail.mit.edu/xiuming' + osp.realpath(args.vis_dir) + '".')
 
-def montage_fig(images, fig_size=(11,4),ncols=11,nrows=4,_permute=True):
+def montage_fig(images, fig_size=(11,4),ncols=11,nrows=4):
     # images list of list of tensors
     fig, ax = plt.subplots(figsize=fig_size,ncols=ncols,nrows=nrows)#该方法会返回画图对象和坐标对象ax，figsize是设置子图长宽（1200，800）
     for x in range(nrows):
         for y in range(ncols):
-            if _permute:
-                images[x][y] = images[x][y][:,:]
             ax[x,y].imshow(images[x][y])
     return fig
 
