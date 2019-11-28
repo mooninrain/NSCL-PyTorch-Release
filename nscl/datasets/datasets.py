7f72eb8d2c905d4c1c3b925c2449da6eb03ce42c
Ruidong Wu <mooninrain@outlook.com>
2019-11-28 18:56:37 -0500

    Update datasets.py

--- nscl/datasets/datasets.py
+++ nscl/datasets/datasets.py
@@ -171,6 +171,7 @@ class NSCLDatsetFilterableView(FilterableDatasetView):
 
         collate_guide = {
             'scene': 'skip',
+            'objects_mask': 'skip',
             'objects_raw': 'skip',
             'objects': 'concat',
 
