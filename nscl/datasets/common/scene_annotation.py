80dd40661b35d824bf732dc6655c966d5c3169f3
Ruidong Wu <mooninrain@outlook.com>
2019-11-28 20:09:01 -0500

    Update scene_annotation.py

--- nscl/datasets/common/scene_annotation.py
+++ nscl/datasets/common/scene_annotation.py
@@ -44,6 +44,17 @@ def annotate_objects(scene):
     boxes[:, 3] += boxes[:, 1]
 
     masks = [mask_utils.decode(i['mask']) for i in _get_object_masks(scene)]
+    masks = [mask_canonize(i) for i in masks]
 
     return {'objects': boxes.astype('float32'), 'objects_mask': masks}
 
+def mask_canonize(mask,slot_num=11):
+    _all_ = np.sum(np.array(mask,dtype=mask[0].dtype),axis=0,dtype=mask[0].dtype)
+    _all_ = (_all_ == 0).astype(_all_.dtype)
+    full_mask = [_all_]+mask
+    full_mask = full_mask[:slot_num]
+    canonized_mask = np.stack(full_mask)
+    if canonized_mask.shape[0] < slot_num:
+        pad_mask = np.zeros((slot_num - canonized_mask.shape[0],)+canonized_mask.shape[1:],dtype=canonized_mask.dtype)
+        canonized_mask = np.concatenate([canonized_mask,pad_mask],axis=0)
+    return canonized_mask        
